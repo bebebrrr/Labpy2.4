16bb10be8438eec72123836aefd12a7e931b7730
bebebrrr <kubanova04@gmail.com>
2023-12-09 19:32:23 +0300

    examples

--- prog/prim2.py
+++ prog/prim2.py
@@ -4,4 +4,22 @@
 import sys
 
 if __name__=='__main__':
-    
+    # ввсети список одной строкой
+    a=list(map(int, input().split()))
+    if not a:
+        print("заданный список пуст", file=sys.stderr)
+        exit(1)
+    a_min=a_max=a[0]
+    i_min=i_max=0
+    for i, item in enumerate(a):
+        if item<a_min:
+            i_min, a_min = i, item
+        if item >= a_max:
+            i_max, a_max= i, item
+    if i_min > i_max:
+        i_min, i_max = i_max, i_min
+    count=0
+    for item in a[i_min+1:i_max]:
+        if item > 0:
+            count +=1
+    print(count)
